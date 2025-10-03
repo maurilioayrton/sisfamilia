485699424d21e1ac3cd92b7dde5e7a8294149189
maurilioayrton <maurilioayrton@hotmail.com>
2025-10-02 23:02:17 -0300

    ajusdando dependencia

--- src/types/react.d.ts
+++ src/types/react.d.ts
@@ -1,12 +1,28 @@
 
 /// <reference types="react" />
 /// <reference types="react-dom" />
+/// <reference types="react/jsx-runtime" />
+
+declare module 'react/jsx-runtime' {
+  export * from 'react/jsx-runtime';
+}
+
+declare module 'react/jsx-dev-runtime' {
+  export * from 'react/jsx-dev-runtime';
+}
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
       [elemName: string]: any;
     }
+    
+    interface Element extends React.ReactElement<any, any> { }
+    interface ElementClass extends React.Component<any> {
+      render(): React.ReactNode;
+    }
+    interface ElementAttributesProperty { props: {}; }
+    interface ElementChildrenAttribute { children: {}; }
   }
 }
 
